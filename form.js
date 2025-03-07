2a0289a0c9a79526d10e7b27d3b932c0eaf57fd8
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-03-07 13:38:06 -0500

    Added logic to all the fields

--- form.js
+++ form.js
@@ -1,24 +1,120 @@
 const form = document.querySelector("#template-form");
 const emailInput = document.querySelector("#form-email");
+const countrySelect = document.querySelector("#form-country");
+const postalCode = document.querySelector("#form-postal-code");
+const passwordEnter = document.querySelector("#form-password");
+const passwordConfirm = document.querySelector('#form-password-confirm');
 const emailError = document.querySelector("#form-email + span.error");
+const countryError = document.querySelector("#form-country + span.error");
+const postalCodeError = document.querySelector("#form-postal-code + span.error");
+const passwordError = document.querySelector("#form-password + span.error");
+const passwordConfirmError = document.querySelector("#form-password-confirm + span.error");
+
+
+//EMAIL validation
 
 emailInput.addEventListener("input", () => {
     if(emailInput.validity.valid) {
         emailError.textContent = "";
         emailError.className = "error";
     } else {
-        showError();
+        showEmailError();
     }
 })
 
+//COUNTRY validation
+
+countrySelect.addEventListener("input", () => {
+    if(countrySelect.validity.valid) {
+        //if country select is valid, error text is nothing. 
+        countryError.textContent = "";
+        countryError.className = "error";
+    } else {
+        showCountryError();
+    }
+})
+
+//POSTAL CODE validation
+
+postalCode.addEventListener("input", () => {
+    if(postalCode.validity.valid) {
+        //if country select is valid, error text is nothing. 
+        postalCode.textContent = "";
+        postalCodeError.className = "error";
+    } else {
+        showPostalCodeError();
+    }
+})
+
+//PASSWORD validation
+
+passwordEnter.addEventListener("input", () => {
+    if(passwordEnter.validity.valid) {
+        //if country select is valid, error text is nothing. 
+        passwordEnter.textContent = "";
+        passwordError.className = "error";
+    } else {
+        showPasswordError();
+    }
+})
+
+//PASSWORD CONFIRM validation
+
+passwordConfirm.addEventListener("input", () => {
+    if(passwordConfirm.value == passwordEnter.value) {
+        //if country select is valid, error text is nothing. 
+        passwordConfirm.textContent = "";
+        passwordConfirmError.className = "error";
+    } else {
+        showPasswordConfirmError();
+    }
+})
+
+//FORM validation
+
 form.addEventListener("submit", (event) =>{
-    if(!emailInput.validity.valid) {
-        showError();
+    if((!emailInput.validity.valid)) {
+        // if the validity is now valid, show the error and prevent form from submit
+        showEmailError();
         event.preventDefault();
     }
 })
 
-function showError() {
+form.addEventListener("submit", (event) =>{
+    if((!countrySelect.validity.valid)) {
+        // if the validity is now valid, show the error and prevent form from submit
+        showCountryError();
+        event.preventDefault();
+    }
+})
+
+form.addEventListener("submit", (event) =>{
+    if((!postalCode.validity.valid)) {
+        // if the validity is now valid, show the error and prevent form from submit
+        showPostalCodeError();
+        event.preventDefault();
+    }
+})
+
+form.addEventListener("submit", (event) =>{
+    if((!passwordEnter.validity.valid)) {
+        // if the validity is now valid, show the error and prevent form from submit
+        showPasswordError();
+        event.preventDefault();
+    }
+})
+
+form.addEventListener("submit", (event) =>{
+    if((passwordConfirm.value != passwordEnter.value)) {
+        // if the validity is now valid, show the error and prevent form from submit
+        showPasswordConfirmError();
+        event.preventDefault();
+    }
+})
+
+//SHOW ERROR functions
+
+function showEmailError() {
     //if you leave it blank
     if (emailInput.validity.valueMissing) {
         emailError.textContent = "You need to enter an email address."
@@ -28,7 +124,48 @@ function showError() {
     //if it doesn't match the minlength that I set
     } else if (emailInput.validity.tooShort) {
         emailError.textContent = `Email should be at least ${emailInput.minLength} characters; you entered ${emailInput.value.length}.`;
-    }
+    } 
 
     emailError.className = "error active";
+        
+}
+
+function showCountryError () {
+    if (countrySelect.validity.valueMissing) {
+        countryError.textContent = "You need to enter a country."
+    } else if (countrySelect.validity.tooShort) {
+        countryError.textContent = `Country should be at least ${countrySelect.minLength} characters; you entered ${countrySelect.value.length}.`
+    } 
+
+    countryError.className = "error active";
+}
+
+function showPostalCodeError () {
+    if (postalCode.validity.valueMissing) {
+        postalCodeError.textContent = "You need to enter a Zip Code."
+    } else if (postalCode.validity.patternMismatch) {
+        postalCodeError.textContent = "Please enter a valid zip code."
+    }
+
+    postalCodeError.className = "error active";
+}
+
+function showPasswordError () {
+    if (passwordEnter.validity.valueMissing) {
+        passwordError.textContent = "You need to enter a password."
+    } else if (passwordEnter.validity.patternMismatch) {
+        passwordError.textContent = "Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters."
+    }
+
+    passwordError.className = "error active";
+}
+
+function showPasswordConfirmError () {
+    if (passwordConfirm.validity.valueMissing) {
+        passwordConfirmError.textContent = "You need to confirm your password."
+    } else if (passwordConfirm.value != passwordEnter.value) {
+        passwordConfirmError.textContent = "Passwords need to match."
+    }
+
+    passwordConfirmError.className = "error active";
 }
